ead379bfe84a9594ec9e5297f540b5173be2e54e
abhinav211203 <krabhinavverma@gmail.com>
2026-08-18 15:13:33 +0530

    add calculator

---
+++ tests/calculator.test.js
@@ -0,0 +1,26 @@
+const {
+    add,
+    subtract,
+    multiply,
+    divide
+} = require("../src/calculator");
+
+test("adds two numbers", () => {
+    expect(add(2, 3)).toBe(5);
+});
+
+test("subtracts two numbers", () => {
+    expect(subtract(5, 3)).toBe(2);
+});
+
+test("multiplies two numbers", () => {
+    expect(multiply(4, 3)).toBe(12);
+});
+
+test("divides two numbers", () => {
+    expect(divide(10, 2)).toBe(5);
+});
+
+test("does not allow division by zero", () => {
+    expect(() => divide(10, 0)).toThrow();
+});
